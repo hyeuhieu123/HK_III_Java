b0d37fafaad10f29a6adb1c3a11c931123dc4f1c
hyeuhieu123 <147390311+hyeuhieu123@users.noreply.github.com>
2025-03-02 19:50:20 -0800

    Update b7.java

--- ss2/b7.java
+++ ss2/b7.java
@@ -1,15 +1,26 @@
-package ss2;
+import java.util.Scanner;
 
 public class b7 {
-	public static void main(String[] args) {
-		int tong =0;
-		for (int i = 1; i <= 60; i++) {
-			if(60 % i ==0){
-				System.out.println(i);
-				tong +=i;
-			}
-		}
-		System.out.println("Tổng từ 1 đến 10 là: " + tong);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        System.out.print("Nhập giờ, phút, giây ban đầu: ");
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int second = sc.nextInt();
+
+        System.out.print("Nhập số giờ, phút, giây cần cộng thêm: ");
+        int addHour = sc.nextInt();
+        int addMinute = sc.nextInt();
+        int addSecond = sc.nextInt();
+
+        int newSecond = second + addSecond;
+        int newMinute = minute + addMinute + (newSecond >= 60 ? newSecond / 60 : 0);
+        newSecond = newSecond % 60;
+
+        int newHour = hour + addHour + (newMinute >= 60 ? newMinute / 60 : 0);
+        newMinute = newMinute % 60;
+
+        System.out.printf("Thời gian sau khi cộng: %02d:%02d:%02d\n", newHour, newMinute, newSecond);
+    }
 }
